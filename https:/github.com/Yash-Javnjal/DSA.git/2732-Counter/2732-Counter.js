3b23a5a9ad784b69941a55fe69cfb211442efe7c
Yash Javanjal <164053746+Yash-Javnjal@users.noreply.github.com>
2026-07-04 14:45:15 +0530

    Create 2732-Counter

---
+++ https:/github.com/Yash-Javnjal/DSA.git/2732-Counter/2732-Counter.js
@@ -0,0 +1,18 @@
+// Last updated: 7/4/2026, 2:45:12 PM
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    
+    return function() {
+        return n++;
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
